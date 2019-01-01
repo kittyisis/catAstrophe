c965f88541253180fa479ddc227097b7af99e5a2
oYoshi <oyoshi@txtsme.com>
2019-01-01 13:50:34 -0500

    Added Cat photos and icons

--- devs.sh
+++ devs.sh
@@ -1,1 +1,1 @@
-pub run build_runner serve web:80 --hostname=192.168.1.101 --hot-reload --delete-conflicting-outputs -v
+pub run build_runner serve web:80 --hostname=192.168.1.103 --hot-reload --delete-conflicting-outputs -v
---
+++ web/static/images/kittyPhotos/downSizeImage.sh
@@ -0,0 +1,8 @@
+convert IMG_0073.JPG -resize 1024x780 img_00.jpg
+convert IMG_2671.JPG -resize 1024x780 img_01.jpg
+convert IMG_2672.JPG -resize 1024x780 img_02.jpg
+convert IMG_2673.JPG -resize 1024x780 img_03.jpg
+convert IMG_2675.JPG -resize 1024x780 img_04.jpg
+convert IMG_2676.JPG -resize 1024x780 img_05.jpg
+convert IMG_2677.JPG -resize 1024x780 img_06.jpg
+convert IMG_2678.JPG -resize 1024x780 img_07.jpg
